ff41bbcb4c120270a8ec22451738a89edd45768c
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-10-26 17:51:43 +0600

    about section added and also banner and navbar section added

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
         <div className="carousel w-full h-[600px]">
             <div id="slide1" className="carousel-item relative w-full">
                 <img src={img1} className="w-full rounded-xl" />
-                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 w-1/2 pl-12'>
                         <h2 className='text-5xl font-bold'>Affordable Price For Car Servicing.</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -27,7 +27,7 @@ const Banner = () => {
             </div>
             <div id="slide2" className="carousel-item relative w-full">
                 <img src={img2} className="w-full rounded-xl" />
-                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 w-1/2 pl-12'>
                         <h2 className='text-5xl font-bold'>Affordable Price For Car Servicing.</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -44,7 +44,7 @@ const Banner = () => {
             </div>
             <div id="slide3" className="carousel-item relative w-full">
                 <img src={img3} className="w-full rounded-xl" />
-                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 w-1/2 pl-12'>
                         <h2 className='text-5xl font-bold'>Affordable Price For Car Servicing.</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
@@ -61,7 +61,7 @@ const Banner = () => {
             </div>
             <div id="slide4" className="carousel-item relative w-full">
                 <img src={img4} className="w-full rounded-xl" />
-                <div className="absolute flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
+                <div className="absolute rounded-xl flex items-center h-full left-0 top-0 bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0.00)]">
                     <div className='text-white space-y-7 w-1/2 pl-12'>
                         <h2 className='text-5xl font-bold'>Affordable Price For Car Servicing.</h2>
                         <p>There are many variations of passages of  available, but the majority have suffered alteration in some form</p>
