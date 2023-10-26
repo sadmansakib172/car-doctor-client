a6ea5f569b8a7b5d6d8e7f5c4e34f8403afcf5e6
sadmansakib172 <salim15-9754@diu.edu.bd>
2023-10-26 18:53:30 +0600

    all single services loaded and showing dynamically done

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
                 </div>
                 <div className='lg:w-1/2 space-y-5 p-4'>
                     <h3 className='text-3xl text-orange-500 font-bold'>About Us</h3>
-                    <h1 className="text-5xl font-bold">We are qualified & of experience in this field</h1>
+                    <h1 className="text-4xl font-bold">We are qualified<br></br> & of experience<br></br> in this field</h1>
                     <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
                     <p>the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
                     <button className="btn btn-warning">Get More Info</button>
